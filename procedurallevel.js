40bddabff2309eaf39dabc4b4f298b9805c0c299
yikuansun <yikuansun2015@gmail.com>
2021-10-16 09:33:00 -0400

    var

--- procedurallevel.js
+++ procedurallevel.js
@@ -1,4 +1,4 @@
-my_level = `........................................
+var my_level = `........................................
 ........................................
 ........................................
 ........................................
@@ -12,25 +12,25 @@ my_level = `........................................
 ########################################`;
 
 function generate_level() {
-    level_split = new Array(12);
-    for (i = 0; i < level_split.length; i++) {
+    var level_split = new Array(12);
+    for (var i = 0; i < level_split.length; i++) {
         level_split[i] = new Array(69);
-        for (j = 0; j < level_split[i].length; j++) {
+        for (var j = 0; j < level_split[i].length; j++) {
             level_split[i][j] = ".";
         }
     }
-    drawx = 1;
-    for (i = 0; i < 5; i++) {
-        building_length = 8 + Math.floor(Math.random() * 3);
-        building_height = 2 + Math.floor(Math.random() * 3);
+    var drawx = 1;
+    for (var i = 0; i < 5; i++) {
+        var building_length = 8 + Math.floor(Math.random() * 3);
+        var building_height = 2 + Math.floor(Math.random() * 3);
         if (i == 0) {
             level_split[level_split.length - 2 - building_height][drawx + 1] = "@";
         }
         if (i == 4) {
             level_split[level_split.length - 2 - building_height][drawx + building_length - 2] = "P";
         }
-        for (j = level_split.length - building_height; j < level_split.length; j++) {
-            for (k = 0; k < building_length; k++) {
+        for (var j = level_split.length - building_height; j < level_split.length; j++) {
+            for (var k = 0; k < building_length; k++) {
                 level_split[j][k + drawx] = "#";
                 if (i > 0 && j == level_split.length - building_height && Math.floor(Math.random() * building_length) < 2) {
                     if (Math.floor(Math.random() * 3)) {
